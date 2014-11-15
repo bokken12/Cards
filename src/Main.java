f37df8a9d1cf623d228a7a6cc2457603e7185a6e
bokken12 <stopwatchj@Joels-iMac.local>
2014-11-15 11:17:29 -0800

    Eblurgh

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.Socket;
 
 import acm.*;
+import acm.program.GraphicsProgram;
 import acm.util.ErrorException;
 
 import java.io.BufferedReader;
@@ -11,34 +13,100 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+import javax.swing.JTextField;
 
+public class Main extends GraphicsProgram{
+
+
+	private static final int PORT_NUMBER = 5002;
+	
+	/* Our preferred size. */
+	public static final int APPLICATION_WIDTH = 500;
+	public static final int APPLICATION_HEIGHT = 600;
+	
+	/* How many columns the input field should have. */
+	private static final int NUM_COLUMNS = 30;
+	
+	/* Input and output streams. */
 	private BufferedReader input;
 	private PrintWriter output;
-
-
-	public void run() {
-		Socket s = setupClient();
-
+	
+	/* Text field where the user can enter text to chat with. */
+	private JTextField text;
+	
+	public void init() {
+		Socket s = connect();
+		println("=== Connection Established! ===");
+		
+		/* Extract the input and output streams from the socket. */
 		try {
 			input = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			output = new PrintWriter(s.getOutputStream());
 		} catch (IOException e) {
 			throw new ErrorException(e);
 		}
+		
+		/* Add the text field to the bottom so that we can chat. */
+		text = new JTextField(NUM_COLUMNS);
+		text.addActionListener(this);
+		add(text, SOUTH);
+	}
+	public void run() {
+		try {
+			/* Continuously read messages from the source. */
+			while (true) {
+				String line = input.readLine();
+				if (line == null) break;
+				
+				println(">>> " + line);
+			}
+		} catch (IOException e) {
+			throw new ErrorException(e);
+		}
+		println("=== Connection Closed ===");
 	}
 
-
-	private Socket setupClient() {
-
+	private Socket connect() {
 		while (true) {
 			try {
-				Socket s = new Socket("Our MegaServer", 554/*random number*/);
+				Socket s = new Socket("Joels-iMac.local", PORT_NUMBER/*random number*/);
 
 				return s;
 			} catch (IOException e) {
 
 			}
 		}
+	}
+	public void fatalError(String errorMessage){
+		closeGame();
+	}
+	public void closeGame(){
+		
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == text) {
+			sendText(text.getText());
+			
+			/* Clear the text box so that we can send another
+			 * message.
+			 */
+			text.setText("");
+		}
+	}
+	
+	/**
+	 * Sends the specified text to the other user.
+	 * 
+	 * @param line The text to send.
+	 */
+	private void sendText(String line) {
+		/* Output the data and flush the output stream to force
+		 * the data to send.
+		 */
+		output.println(line);
+		output.flush();
+		
+		/* For our sanity, display the text we sent. */
+		println("<<< " + line);
 	}
 }
--- src/MegaServer/MegaServer.java
+++ src/MegaServer/MegaServer.java
@@ -2,47 +2,74 @@ package MegaServer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Handler;
 
 public class MegaServer {
-
+	static ArrayList<PrintWriter> writers = new ArrayList<PrintWriter>();
+	static int PORT_NUMBER = 5002;
 	public static void main(String[] args) {
-		
+
 		ServerSocket listener = null;
 		try {
-		listener = new ServerSocket(554);
-        
-            while (true) {
-                new Handler(listener.accept()).start();
-            }
-        } catch (IOException e) {
+			listener = new ServerSocket(PORT_NUMBER);
+			System.out.println("Waiting for a connection.");
+
+			while (true) {
+				new Handler(listener.accept()).start();
+			}
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-            try {
+			try {
 				listener.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
-		
-	}
-	 private static class Handler extends Thread {
-	        private String name;
-	        private Socket socket;
-	        private BufferedReader in;
-	        private PrintWriter out;
+		}
 
-	        /**
-	         * Constructs a handler thread, squirreling away the socket.
-	         * All the interesting work is done in the run method.
-	         */
-	        public Handler(Socket socket) {
-	            this.socket = socket;
-	        }
-	 }
+	}
+	private static class Handler extends Thread {
+		private String name;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+
+		/**
+		 * Constructs a handler thread, squirreling away the socket.
+		 * All the interesting work is done in the run method.
+		 */
+		public Handler(Socket socket) {
+			this.socket = socket;
+		}
+		public void run() {
+			try {
+				System.out.println("Got a connection");
+				// Create character streams for the socket.
+				in = new BufferedReader(new InputStreamReader(
+						socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
+				writers.add(out);
+				// Request a name from this client.  Keep requesting until
+				// a name is submitted that is not already used.  Note that
+				// checking for the existence of a name and adding the name
+				// must be done while locking the set of names.
+				while (true) {
+					String line = in.readLine();
+					if(line == null) break;
+					for(PrintWriter pw: writers){
+						pw.println(">>> " + line);
+					}
+				}
+			} catch(Exception e) {
+
+			}
+		}
+	}
 }
