db5349c25bca5f396bf92e5c66c8fcfef2480f0d
bokken12 <stopwatchj@gmail.com>
2015-06-19 10:40:22 -0700

    im an idiot

--- src/abilities/Ability.java
+++ src/abilities/Ability.java
@@ -1,16 +1,18 @@
 package abilities;
 
+import events.EventBus;
 import events.GameEvent;
 import events.GameListener;
 
 public class Ability implements GameListener{
 	String name;
 	String description;
-	GameEvent activation;
+	Class<GameEvent> activation;
 	GameEvent callevent;
 	public Ability(){
 		
 	}
 	public void passEvent(GameEvent event){
+		EventBus.getBus().callEvent(callevent);
 	}
 }
--- src/events/GameEvent.java
+++ src/events/GameEvent.java
@@ -1,6 +1,9 @@
 package events;
 
+import java.util.ArrayList;
+
 public abstract class GameEvent {
+	private static ArrayList<GameListener> listeners = new ArrayList<GameListener>();
 	boolean isCancelled = false;
 	public abstract void fireEvent();
 	public void setCancelled(boolean cancelled){
@@ -8,5 +11,7 @@ public abstract class GameEvent {
 	}
 	public boolean isEventCancelled(){
 		return isCancelled;
+	}
+	public static void addListener(){
 	}
 }
