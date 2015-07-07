ce600bfc4c574a58443cf0b3bdb5e21c1dcb8838
Joel Manning <stopwatchj@gmail.com>
2015-07-06 19:27:07 -0700

    Changed the Event Bus

--- src/events/EventBus.java
+++ src/events/EventBus.java
@@ -5,12 +5,13 @@ import java.util.HashMap;
 
 public class EventBus {
 	static EventBus currentbus;
-	HashMap<Class<GameEvent>, HashMap<Integer, ArrayList<GameListener>>> listening = new HashMap<Class<GameEvent>, HashMap<Integer, ArrayList<GameListener>>>();
-	public void addGameListener(Integer priority, Class<GameEvent> e, GameListener l){
+	static HashMap<Class<? extends GameEvent>, HashMap<Integer, ArrayList<GameListener>>> listening = new HashMap<Class<? extends GameEvent>, HashMap<Integer, ArrayList<GameListener>>>();
+	@SuppressWarnings("unchecked")
+	public static void addGameListener(Integer priority, Class<? extends GameEvent> e, GameListener l){
 		if(priority.intValue() > 20 || priority.intValue() < 0){
 			throw new IllegalArgumentException("You may only have priorities between 0 and 20");
 		}
-		for(Class<GameEvent> c = e; !(c.equals(GameEvent.class)); c = (Class<GameEvent>) c.getSuperclass()){
+		for(Class<? extends GameEvent> c = e; !(c.equals(GameEvent.class)); c = (Class<? extends GameEvent>) c.getSuperclass()){
 			if(listening.containsKey(c)){
 				if(listening.get(c).containsKey(priority)){
 					listening.get(c).get(priority).add(l);
@@ -26,9 +27,12 @@ public class EventBus {
 				h.put(priority, a);
 				listening.put(c, h);
 			}
+			if(c.getSuperclass().equals(GameEvent.class)){
+				break;
+			}
 		}
 	}
-	public void callEvent(GameEvent e){
+	public static void callEvent(GameEvent e){
 		if(listening.containsKey(e.getClass())){
 			for(int i = 0; i <= 20; i++){
 				if(listening.get(e).containsKey(i)){
