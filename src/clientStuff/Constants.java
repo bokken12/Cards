627eeaba2e27c69f1cf8c39106e115ee92fd3eb9
Joel Manning <stopwatchj@gmail.com>
2015-11-29 15:50:39 -0800

    Revert "joel is and idiot"
    This reverts commit 69fc3149e960e2b108a4ab69b97236579c54c75f.

--- src/clientStuff/Constants.java
+++ src/clientStuff/Constants.java
@@ -5,4 +5,16 @@ public interface Constants {
 	public static final int LANE_1 = 1;
 	public static final int LANE_2 = 2;
 	public static final int LANE_3 = 3;
+    public static final int LANE_1_START_X = 51;
+    public static final int LANE_1_START_Y = 67;
+    public static final int LANE_1_END_X = 305;
+    public static final int LANE_1_END_Y = 600;
+    public static final int LANE_2_START_X = 473;
+    public static final int LANE_2_START_Y = 110;
+    public static final int LANE_2_END_X = 700;
+    public static final int LANE_2_END_Y = 600;
+    public static final int LANE_3_START_X = 855;
+    public static final int LANE_3_START_Y = 115;
+    public static final int LANE_3_END_X = 1113;
+    public static final int LANE_3_END_Y = 600;
 }
