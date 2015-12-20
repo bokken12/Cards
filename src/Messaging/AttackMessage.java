9a58b948935eabb2cbd65d48b7c26a4d4f138d8d
Casey Manning <kcgrind@gmail.com>
2015-12-20 13:54:18 -0800

    xample

---
+++ src/Messaging/AttackMessage.java
@@ -0,0 +1,16 @@
+package Messaging;
+
+public class AttackMessage extends Message {
+	
+	public AttackMessage fromString(String s) {
+		return null;
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/Messaging/CreaturePlayedMessage.java
@@ -0,0 +1,17 @@
+package Messaging;
+
+public class CreaturePlayedMessage extends Message {
+
+	@Override
+	public Message fromString(String args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
