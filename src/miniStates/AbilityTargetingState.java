af09e2936d13f964145b6563b21aebc2dad31eb2
Joel Manning <stopwatchj@gmail.com>
2015-12-28 08:04:33 -0800

    All the states I think we need for now

---
+++ src/miniStates/AbilityTargetingState.java
@@ -0,0 +1,75 @@
+package miniStates;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+import messaging.Message;
+import clientStates.GameState;
+
+public class AbilityTargetingState extends MiniState
+{
+
+    @Override
+    public void MessageRecieved(Message message)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onBegin(GameState stater)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onLeave(GameState stater)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
